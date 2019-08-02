ae5de762d55297d8852b60a9ab2531bde07c5a5e
fer638 <53579568+fer638@users.noreply.github.com>
2019-08-01 21:08:23 -0500

    Delete PanelPrincipal.py

--- PanelPrincipal.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-from Frame import FramePrincipal
-
-ventana = Tk()
-ventana.title("Nueva Ventana")
-ventana.geometry('350x200')
-frame = FramePrincipal()
-
-ventana.mainloop()
